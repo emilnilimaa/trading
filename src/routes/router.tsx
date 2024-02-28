1f786e8bc099c6a5fd3d612df4238dd9e38fc8c3
Emil Nilimaa <84347991+emilnilimaa@users.noreply.github.com>
2024-02-28 19:51:28 +0100

    Refactor router some

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,30 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import HomePage from "../pages/HomePage";
-import Layout from "./Layout";
 import { RequireAuth } from "./RequireAuth";
 import Root from "./Root";
 
-const pages: any[] = [];
+const pages: any[] = [
+  {
+    path: "/",
+    element: (
+      <RequireAuth>
+        <HomePage />
+      </RequireAuth>
+    ),
+  },
+];
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: (
-          <RequireAuth>
-            <Layout>
-              <HomePage />
-            </Layout>
-          </RequireAuth>
-        ),
-      },
-      ...pages,
-    ],
+    children: pages,
   },
 ]);
 export default router;
