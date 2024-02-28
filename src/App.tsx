74c471ac5acd8f67907de45e9c020ec673768ba4
Emil Nilimaa <84347991+emilnilimaa@users.noreply.github.com>
2024-02-28 19:29:17 +0100

    Fix app root with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import router from "./routes/router";
+
+const Fallback = () => {
+  return null;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Fallback />}>
+      <RouterProvider router={router} />
+    </Suspense>
   );
 }
 
