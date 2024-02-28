540244940bfec457112eabd1b25cae30c5e91c13
Emil Nilimaa <84347991+emilnilimaa@users.noreply.github.com>
2024-02-28 19:32:32 +0100

    Fix error text

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,7 +2,7 @@ const ErrorPage = () => {
   return (
     <div>
       <h1>Error</h1>
-      <p>Oops! An error occurred.</p>
+      <p>Oops! An error occurred, or page not found.</p>
     </div>
   );
 };
