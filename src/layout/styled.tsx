d979cc66935d586ae11f0205abc54908a7837176
Emil Nilimaa <84347991+emilnilimaa@users.noreply.github.com>
2024-02-28 20:23:48 +0100

    Basic layout styles

---
+++ src/layout/styled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const LayoutWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const LayoutContainer = styled.div`
+  max-width: 680px;
+  min-width: 360px;
+  width: 680px;
+`;
