6b8621525c9f74559b567ad9db1b48c604c8efc9
Emil Nilimaa <84347991+emilnilimaa@users.noreply.github.com>
2024-02-29 01:08:48 +0100

    Use layout in root component

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import Header from "../components/Header";
+import Menu from "../components/Menu";
+import { LayoutContainer, LayoutWrapper } from "../layout/styled";
 
 const FullHeightPageContainer = styled.div`
   height: 100%;
   background-color: ${(props) => props.theme.bodyBackground};
-  //margin-top: -56px;
-  padding-top: 56px;
-  //padding-left: 20px;
-  //padding-right: 20px;
+  padding-top: 29px;
 
   @media (max-width: 720px) {
     padding-left: 0px;
@@ -20,7 +19,12 @@ export default function Root() {
   return (
     <FullHeightPageContainer>
       <Header />
-      <Outlet />
+      <Menu />
+      <LayoutWrapper>
+        <LayoutContainer>
+          <Outlet />
+        </LayoutContainer>
+      </LayoutWrapper>
     </FullHeightPageContainer>
   );
 }
