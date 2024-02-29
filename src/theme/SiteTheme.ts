09ec91869df25e974bd1c7720518d462c73ded9d
Emil Nilimaa <84347991+emilnilimaa@users.noreply.github.com>
2024-02-29 01:09:14 +0100

    Updated theme

--- src/theme/SiteTheme.ts
+++ src/theme/SiteTheme.ts
@@ -4,14 +4,32 @@ declare module "@mui/material/styles" {
   interface Theme {
     name: string;
     headerBg: string;
+    linkColor: string;
+    colors: {
+      primary: string;
+      white: string;
+      red: string;
+    };
   }
   // allow configuration using `createTheme`
   interface ThemeOptions {
     name: string;
     headerBg: string;
+    linkColor: string;
+    colors: {
+      primary: string;
+      white: string;
+      red: string;
+    };
   }
 }
 export const lightTheme = createTheme({
   name: "light",
   headerBg: "#f7f7f7",
+  linkColor: "#059b72",
+  colors: {
+    primary: "#059b72",
+    white: "#ffffff",
+    red: "#d0184d",
+  },
 });
